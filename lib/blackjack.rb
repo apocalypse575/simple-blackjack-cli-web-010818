6ae5b6c3cc4f31c1427a84dd55a4e404debfd86a
apocalypse575 <apocalypse575@gmail.com>
2017-12-14 00:57:05 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,16 +49,6 @@ def invalid_command
   puts "Please enter a valid command"
 end
 
-def runner
-  welcome
-  total = initial_round
-until total >= 22
-  total = hit?(total)
-  display_card_total(total)
-end
-  end_game(total)
-end
-
 
 #####################################################
 # get every test to pass before coding runner below #
