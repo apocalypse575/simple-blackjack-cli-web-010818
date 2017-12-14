7fe4fc44c2094c3be641997bb40936626a799602
apocalypse575 <apocalypse575@gmail.com>
2017-12-14 00:57:45 +0000

    automatically backed up by learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,11 +1,3 @@
 require_relative "blackjack.rb"
 
-def runner
-  welcome
-  total = initial_round
-until total >= 22
-  total = hit?(total)
-  display_card_total(total)
-end
-  end_game(total)
-end
+runner
